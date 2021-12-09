472151f2d18ebf33e14abfbaf6b06d37d48ab484
ahron <asmillrod@gmail.com>
2021-12-09 11:06:30 -0500

    Section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 
 function App() {
-
-  // const hi = () => {
-    // }
-    
     const [activities, setActivities] = useState([]);
+
     
     useEffect(() => {
     console.log(activities)
@@ -19,19 +17,17 @@ function App() {
     })
   }, [])
 
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>hi</h2>
-       <ul>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+
+       <List>
          {activities.map((activity: any) => (
-           <li key={activity.id}>
+           <List.Item key={activity.id}>
              {activity.title}
-           </li>
+           </List.Item>
          ))}
-       </ul>
-      </header>
+       </List>
     </div>
   );
 }
